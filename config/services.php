e1cb85eb5e247b0a03aee3b7edbf7568086132b3
fatemesoleymanian <soleymanian.usc@gmail.com>
2025-03-16 14:07:26 +0330

    Update services.php for ghasedak

--- config/services.php
+++ config/services.php
@@ -30,5 +30,8 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'ghasedak' => [
+    'api_key' => env('GHASEDAK_API_KEY'),
+],
 
 ];
